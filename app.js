5c8ef3c2d1117bb3fe78a73b8d32c890a91e1598
nitish-002 <surya0424sharma@gmail.com>
2025-04-24 14:15:22 +0530

    updated app.js

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ const bodyParser = require('body-parser');
 const app = express();
 
 // Database
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect('mongodb://mongodb:27017/quotesDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-.then(() => console.log('Successfully connected to MongoDB Atlas'))
-.catch((error) => console.error('Error connecting to MongoDB Atlas:', error));
+.then(() => console.log('Successfully connected to MongoDB'))
+.catch((error) => console.error('Error connecting to MongoDB:', error));
 
 // Middleware
 app.use(bodyParser.json());
 
 // Routes
 app.get('/', (req, res) => {
-	res.send("Hello, World!");
+    res.send("Hello, World!");
 });
 
 const QuotesRoute = require('./routes/Quotes');
@@ -27,4 +27,5 @@ const QuotesRoute = require('./routes/Quotes');
 app.use('/quotes', QuotesRoute);
 
 // Starting server
-app.listen(3000, console.log("Listening on port 3000"));
+const PORT =3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
